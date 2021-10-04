aefad4378bcc98165ef37fd9ec6abe5bdbd0bac1
EranosyanEA <EranosyanEA@yandex.ru>
2021-10-04 23:57:53 +0300

    feat: добавить поддержку google maps

--- app/src/main/java/com/example/gb_my_app/ui/view/ActorFragment.kt
+++ app/src/main/java/com/example/gb_my_app/ui/view/ActorFragment.kt
@@ -1,6 +1,7 @@
 package com.example.gb_my_app.ui.view
 
 import android.content.Context
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gb_my_app.AppState
+import com.example.gb_my_app.R
 import com.example.gb_my_app.databinding.ActorFragmentBinding
 import com.example.gb_my_app.model.Actor
 import com.example.gb_my_app.utils.convertToHumanDate
 import com.example.gb_my_app.utils.showSnackbar
 import com.example.gb_my_app.utils.toVisibility
 import com.example.gb_my_app.view_model.ActorViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import kotlin.concurrent.thread
 
 private const val ARG_ACTOR_ID = "ACTOR_ID"
 
@@ -31,9 +41,20 @@ class ActorFragment : Fragment() {
         ViewModelProvider(this).get(ActorViewModel::class.java)
     }
 
+    private lateinit var map: GoogleMap
+
     private var paramActorID: Int? = null
 
+    // Callbacks
     private var callbacks: MainFragment.Callbacks? = null
+
+    private val mapCallback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     // View binding
     private var viewBindingRef: ActorFragmentBinding? = null
@@ -65,6 +86,9 @@ class ActorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(mapCallback)
+
         viewModel.actorLiveData.observe(viewLifecycleOwner, ::render)
         viewModel.getActorById(paramActorID)
     }
@@ -72,6 +96,30 @@ class ActorFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         viewBindingRef = null
+    }
+
+    private fun initPlaceOfBirth(placeOfBirth: String) {
+        val geocoder = Geocoder(context)
+
+        thread {
+            try {
+                val addressList = geocoder.getFromLocationName(placeOfBirth, 1)
+
+                if (addressList.isNotEmpty()) {
+                    val address = addressList[0]
+                    val location = LatLng(address.latitude, address.longitude)
+
+                    view?.post {
+                        map.also {
+                            it.addMarker(MarkerOptions().position(location).title(placeOfBirth))
+                            it.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+                        }
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun render(appState: AppState) {
@@ -91,6 +139,8 @@ class ActorFragment : Fragment() {
                     vb.actorName.text = a.name
                     vb.actorBirthday.text = a.birthday.convertToHumanDate()
                     vb.actorPopularity.text = a.popularity.toString()
+
+                    initPlaceOfBirth(a.placeOfBirth)
                 }
 
                 callbacks?.onShowProgress(View.INVISIBLE)
